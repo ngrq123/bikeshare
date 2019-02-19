e5ae919182d105cb047f9c4ee8907e29e9de1a8e
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2019-02-19 14:03:17 +0100

    Version 4: BikeShareFragment from BikeShareActivity

--- app/src/main/java/dk/itu/mmad/bikeshare/BikeShareActivity.java
+++ app/src/main/java/dk/itu/mmad/bikeshare/BikeShareActivity.java
@@ -1,17 +1,11 @@
 package dk.itu.mmad.bikeshare;
 
-import android.content.Intent;
-import android.os.Build;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
-
-import java.util.List;
-
-import dk.itu.mmad.bikeshare.R;
 
 public class BikeShareActivity extends AppCompatActivity {
 
@@ -30,34 +24,15 @@ public class BikeShareActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bike_share);
 
-        // Button
-        mAddRide = (Button) findViewById(R.id.add_button);
-        mEndRide = (Button) findViewById(R.id.end_button);
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
-        // Click events
-        mAddRide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(BikeShareActivity.this, StartRideActivity.class);
-                startActivity(intent);
-            }
-        });
+        if (fragment == null) {
+            fragment = new BikeShareFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
 
-        mEndRide.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(BikeShareActivity.this, EndRideActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        // Singleton to share an object between the app activities
-        sRidesDB = RidesDB.get(this);
-        List<Ride> values = sRidesDB.getRidesDB();
-
-        // Create the adaptor
-        mAdaptor = new RideArrayAdaptor(this, values);
-        mListView = (ListView) findViewById(R.id.main_list_view);
-        mListView.setAdapter(mAdaptor);
     }
 }
---
+++ app/src/main/java/dk/itu/mmad/bikeshare/BikeShareFragment.java
@@ -0,0 +1,70 @@
+package dk.itu.mmad.bikeshare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.List;
+
+public class BikeShareFragment extends Fragment {
+    // GUI variables
+    private Button mAddRide;
+    private Button mEndRide;
+
+    private Ride mLast = new Ride("", "", "");
+
+    private static RidesDB sRidesDB;
+    private RideArrayAdaptor mAdaptor;
+    private ListView mListView;
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater, final ViewGroup container, Bundle savedBundleState) {
+        View v = inflater.inflate(R.layout.fragment_bike_share, container, false);
+        // Button
+        mAddRide = (Button) v.findViewById(R.id.add_button);
+        mEndRide = (Button) v.findViewById(R.id.end_button);
+
+        // Click events
+        mAddRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), StartRideActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mEndRide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), EndRideActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // Singleton to share an object between the app activities
+        sRidesDB = RidesDB.get(getContext());
+        List<Ride> values = sRidesDB.getRidesDB();
+
+        // Create the adaptor
+        mAdaptor = new RideArrayAdaptor(getContext(), values);
+        mListView = (ListView) v.findViewById(R.id.main_list_view);
+        mListView.setAdapter(mAdaptor);
+
+        return v;
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_bike_share);
+
+    }
+
+
+}
