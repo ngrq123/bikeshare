b0c77c8525fb3e3f2d2a12284c68ae00c9d250df
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2019-04-27 16:53:30 +0200

    Version 8: Added account balance in list of transactions and minor layout changes

--- app/src/main/java/dk/itu/mmad/bikeshare/controller/ListTransactionsActivity.java
+++ app/src/main/java/dk/itu/mmad/bikeshare/controller/ListTransactionsActivity.java
@@ -5,15 +5,18 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.TextView;
 
 import dk.itu.mmad.bikeshare.R;
 import dk.itu.mmad.bikeshare.model.Bike;
 import dk.itu.mmad.bikeshare.model.Transaction;
+import dk.itu.mmad.bikeshare.model.User;
 import io.realm.Realm;
 import io.realm.Sort;
 
 public class ListTransactionsActivity extends AppCompatActivity {
 
+    private TextView mAccountBalanceText;
     private RecyclerView mRecyclerView;
     private TransactionAdaptor mAdaptor;
 
@@ -21,6 +24,22 @@ public class ListTransactionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_transactions);
+
+        mAccountBalanceText = (TextView) findViewById(R.id.account_balance_text);
+
+        try (Realm realm = Realm.getDefaultInstance()) {
+            realm.executeTransactionAsync(new Realm.Transaction() {
+                @Override
+                public void execute(Realm bgRealm) {
+                    User user = bgRealm.where(User.class)
+                            .equalTo("mEmail", PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                                    .getString("user", null))
+                            .findFirst();
+                    String accountBalanceText = "Account balance: " + user.getBalance() + " kr";
+                    mAccountBalanceText.setText(accountBalanceText);
+                }
+            });
+        }
 
         mRecyclerView = (RecyclerView) findViewById(R.id.transaction_list_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
