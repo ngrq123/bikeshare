2383aa8c255144934198638839b254a63cdfe95f
NG Rui Qin <ruiqin.ng.2016@sis.smu.edu.sg>
2019-04-27 22:36:11 +0200

    Version 8: Add toggle to show only free bikes in list bikes

--- app/src/main/java/dk/itu/mmad/bikeshare/controller/ListBikesActivity.java
+++ app/src/main/java/dk/itu/mmad/bikeshare/controller/ListBikesActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.CheckBox;
 
 import dk.itu.mmad.bikeshare.R;
 import dk.itu.mmad.bikeshare.model.Bike;
@@ -12,6 +14,7 @@ import io.realm.Realm;
 
 public class ListBikesActivity extends AppCompatActivity {
 
+    private CheckBox mOnlyFreeCheckBox;
     private RecyclerView mRecyclerView;
     private BikeAdaptor mAdaptor;
 
@@ -19,6 +22,23 @@ public class ListBikesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_bikes);
+
+        mOnlyFreeCheckBox = (CheckBox) findViewById(R.id.only_free_check_box);
+        mOnlyFreeCheckBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try (Realm realm = Realm.getDefaultInstance()) {
+                    if (mOnlyFreeCheckBox.isChecked()) {
+                        mAdaptor = new BikeAdaptor(realm.where(Bike.class)
+                                .equalTo("mIsInUse", false)
+                                .findAllAsync());
+                    } else {
+                        mAdaptor = new BikeAdaptor(realm.where(Bike.class).findAllAsync());
+                    }
+                    mRecyclerView.setAdapter(mAdaptor);
+                }
+            }
+        });
 
         mRecyclerView = (RecyclerView) findViewById(R.id.bike_list_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
